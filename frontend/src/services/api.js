bd8db423b050f2342909aef3db6347746ae3b627
MMZ3R0 <mmoura_np@hotmail.com>
2021-09-23 20:24:38 -0300

    Adição do GoogleChart para construção do grafico

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+require('dotenv').config();
 
 const api = axios.create({
-    baseURL: 'http://localhost:3005/',
+    baseURL: 'http://192.168.1.10:3005/api/',
 })
 
 export default api;
